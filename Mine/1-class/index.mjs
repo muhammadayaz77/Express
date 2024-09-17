2c94169de6feece5eb52c6da07ea0c9a345e1bd7
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-17 19:43:38 +0500

    add middleware

--- Mine/1-class/index.mjs
+++ Mine/1-class/index.mjs
@@ -1,13 +1,23 @@
 import express from "express";
 const app = express();
-
+app.use((req,res,next) => {
+  console.log('Middleware chala');
+  next();
+})
+app.use((req,res,next) => {
+  console.log('Middleware chala per se');
+  next();
+})
 app.get('/',(req,res) => {
   res.send('Home Page')
 })
-app.get('/profile',(req,res) => {
-  res.send('Profile Page')
+app.get('/profile',(req,res,next) => {
+  return next(new Error('Something went wrong'))
 })
-
-app.listen(300,() => {
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+app.listen(3000,() => {
   console.log('http://localhost:3000')
 })
