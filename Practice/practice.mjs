3610481ad9f4b891b590f2d9b30b0d33434bceb5
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-19 15:00:19 +0500

    practice express intro

--- Practice/practice.mjs
+++ Practice/practice.mjs
@@ -1,23 +1,42 @@
-import express from "express"
+// import express from "express"
+
+// let app = express();
+// app.use(express.json())
+// app.get('/api/data',(req,res) => {
+//   res.json({message : 'Home Page'});
+// })
+// app.post('/api/data',(req,res) => {
+//   console.log(req.body)
+//   res.json({message : 'Home Page',body : req.body});
+// })
+// app.put('/api/data/:ids',(req,res) => {
+//   console.log(req.params.ids)
+//   res.json({message : 'Home Page',body : req.body,id : req.params.ids});
+// })
+// app.delete('/api/data/:ids',(req,res) => {
+//   console.log(req.params.ids)
+//   res.json({message : 'Home Page',body : req.body,id : req.params.ids});
+// })
+
+// app.listen(3000,() => {
+//   console.log('http://localhost:3000')
+// })
+
+
+import express from 'express'
 
 let app = express();
-app.use(express.json())
-app.get('/api/data',(req,res) => {
+app.use(express.json());
+app.get('/',(req,res) => {
   res.json({message : 'Home Page'});
 })
-app.post('/api/data',(req,res) => {
-  console.log(req.body)
-  res.json({message : 'Home Page',body : req.body});
+app.post('/',(req,res) => {
+  res.json({data : req.body});
 })
-app.put('/api/data/:ids',(req,res) => {
-  console.log(req.params.ids)
-  res.json({message : 'Home Page',body : req.body,id : req.params.ids});
-})
-app.delete('/api/data/:ids',(req,res) => {
-  console.log(req.params.ids)
-  res.json({message : 'Home Page',body : req.body,id : req.params.ids});
+app.put('/:id',(req,res) => {
+  res.json({id : req.params.id,data : req.body});
 })
 
 app.listen(3000,() => {
-  console.log('http://localhost:3000')
+  console.log(`http://localhost:3000`);
 })
