5c995ad809b2dc357f82f89cdbaad3fc9319c47c
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-09-22 10:33:22 +0500

    add crud with array

--- 02 Class/index.mjs
+++ 02 Class/index.mjs
@@ -1,16 +1,43 @@
 import express from 'express'
+// first commit 
 
 let app = express();
-let users = [
-  {id : 1,name : 'ali',email : 'ali@gmail.com'},
-]
+app.use(express.json())
+// second commit
+let items = []
 app.get('/',(req,res) => {
-  res.status(200).json({users});
+  res.json(items);
 })
-app.post('/api/users',(req,res) => {
-  res.json({message : 'data is posted',data : req.body});
+// third commit
+app.post('/api/data',(req,res) => {
+  // Forth commit
+  let newItem = {
+      id : items.length + 1,
+      name : req.body.name,
+  }
+  items.push(newItem);
+  res.status(201).json({message : 'data is Post',newItem});
+  // Fifth commit
 })
+app.delete('/api/data/:id',(req,res) => {
+    let itemIndex = items.findIndex(i => i.id === parseInt(req.params.id));
+    // Sixth commit 
+    if(itemIndex == -1) return res.status(404).json({message : `item at index : ${itemIndex} not found`})
 
+    let deletedItem = items.splice(itemIndex,1);
+    res.json(deletedItem);
+    // seventh commit
+})
+app.put('/api/data/:id',(req,res) => {
+  // eighth commit 
+  const index = parseInt(req.params.id - 1);
+  if (index < 0 || index >= items.length) return res.status(404).send('Item not found.');
+  // tenth commit 
+  const updateItem = {id : items[index].id,...req.body};
+  items[index] = updateItem;
+  res.json(updateItem)
+  // ninth commit
+})
 app.listen(3000,() => {
-  console.log('http://localhost:3000')
+  console.log('http://localhost:3000');
 })
