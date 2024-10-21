3aec0569db775d264fd18db5d89cbad97b921a61
Muhammad Ayaz <muhammadayaz22757@gmail.com>
2024-10-21 23:37:46 +0500

    rev jwt

--- Mine/4-auth/app.mjs
+++ Mine/4-auth/app.mjs
@@ -1,15 +1,17 @@
 import cookieParser from 'cookie-parser';
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
 let app = express();
 app.use(cookieParser());
 app.get("/",(req,res) => {
-  res.cookie("name","ayaz");
-  res.send("Done");
-})
+  let token = jwt.sign({email : "ayaz@gmail.com"},"secret");
+  console.log(token)
+});
 app.get("/read",(req,res) => {
-  console.log(req.cookies)
-  res.send("read")
+ res.send("Ok")
 })
+
 app.listen(3000,() => {
   console.log(`http://localhost:3000`);
 })
